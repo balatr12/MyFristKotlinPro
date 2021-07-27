9a81f19b316c2692c5542261d48204ed001786c9
bala <BR00000@safeway.com>
2021-07-27 11:09:39 +0530

    second commit

--- app/src/main/java/com/ibm/kotlinpro/FirstFragment.kt
+++ app/src/main/java/com/ibm/kotlinpro/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.ibm.kotlinpro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +26,7 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        Log.d("FRAGMENT", "FIRST FRAGMENT")
         return binding.root
 
     }
